b3047b7214cfafce934958e060a89fe13db6683b
Paul-Jasper Sahr <pauljasper.sahr@gmail.com>
2024-05-10 16:24:47 +0200

    basic project setup

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  pages: true,
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@pinia/nuxt",
+    "@nuxtjs/supabase",
+    "@vueuse/nuxt",
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { supabase } from "../utils/supabase";
+const todos = ref([]);
+
+async function getTodos() {
+  const { data: todos } = await supabase.from("todos").select();
+  countries.value = todos;
+}
+
+onMounted(() => {
+  getTodos();
+});
+</script>
+
+<template>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">{{ todo.name }}</li>
+  </ul>
+</template>
